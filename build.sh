629f4232655d0b0b3688a6dd218cb49761f2d26a
Adam Kessel <adam@rosi-kessel.org>
2026-04-26 16:13:39 -0400

    error checking on build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 rm -r dist/
-pyinstaller --noconfirm ./gedcom-dna-finder-cli.spec
-pyinstaller --noconfirm ./gedcom-dna-finder-gui.spec
+pyinstaller --noconfirm ./gedcom-dna-finder-cli.spec || { echo 'pyinstaller failed to build CLI.'; exit 1; }
+pyinstaller --noconfirm ./gedcom-dna-finder-gui.spec || { echo 'pyinstaller failed to build GUI.'; exit 1; }
+[ -d "dist" ] || { echo 'Cannot find dist build folder.'; exit 1; }
+pushd dist
 if [[ $(uname) == "Linux" ]]
 then 
-zip -r gedcom-dna-finder-linux.zip dist/*
+zip -r - . > ../gedcom-dna-finder-linux.zip
 else
-zip -r gedcom-dna-finder-mac.zip dist/*
+zip -r - . > ../gedcom-dna-finder-mac.zip 
 fi
+popd
