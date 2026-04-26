8acde6e277844fc0e89bb0f7cb8ad562ecf52030
Adam Kessel <adam@rosi-kessel.org>
2026-04-26 07:23:12 -0500

    windows build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 pyinstaller --onefile --icon=family_tree.ico .\gedcom-dna-finder-cli.py
 pyinstaller --onefile --windowed --icon=family_tree.ico .\gedcom-dna-finder-gui.py
+compress-archive -path dist\* -destinationpath .\gedcom-dna-finder.zip
