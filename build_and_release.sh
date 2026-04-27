905b2a557bf6b9b2c975d9354316010957ea30b0
Adam Kessel <ajkessel@gmail.com>
2026-04-27 15:51:00 -0400

    typo

--- build_and_release.sh
+++ build_and_release.sh
@@ -14,7 +14,7 @@ echo 'Building for Linux platform...'
 echo 'Building for Mac platform...'
 ssh vmac 'cd gedcom-dna-finder/ ; git pull ; source .venv/bin/activate ; ./build.sh'
 echo 'Building for Windows platform...'
-pwsh -command 'set-location c:/apps/src/gedcom-dna-finder ; git pull ; venv ; ./.venv/scripts/activate ; ./build.ps1' 
+pwsh -command 'set-location c:/apps/src/gedcom-dna-finder ; git pull ; venv ; ./build.ps1' 
 echo 'Copying built ZIP files locally...'
 scp vmac:gedcom-dna-finder/*zip . 
 cp /mnt/c/apps/src/gedcom-dna-finder/*zip . 
